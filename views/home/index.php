53caf1c070585008a729d0c7abcc766b4542418c
Agustin Servin <agustinservin1@gmail.com>
2022-09-16 09:25:38 -0300

    actualizo el home, home muestra los autos de la db y modifico algunos estilos

--- views/home/index.php
+++ views/home/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     <?php
@@ -12,17 +13,21 @@
     ?>
 
     <div id="main">
-        <h1>Bienvenido al sitio</h1>
         <?php
-            foreach($this->autos as $auto){
-                foreach($auto as $key => $value){
-                    if($key == "foto"){
+            foreach($this->autos as $row){
+               $auto = new Auto();
+               $auto = $row;
                         ?>
-                        <img src="<?php echo $value; ?>" alt="">
+  <div class="card">
+  <h5 class="card-title"><?php echo $auto->nombre." ".$auto->modelo; ?></h5>
+    <img width="259" height="180" class="card-img-top" src="<?php echo $auto->foto; ?>" alt="Card image cap">
+    <div class="card-body">
+     
+      <button class="botones in-card">Ver modelo</button><button class="botones in-card">Consultar precio</button>
+    </div>
+  </div>
                         <?php
                     }
-                }
-            }
         ?>
     </div>
 
