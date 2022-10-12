9b0a7288bcb7d1117f1c20ccc2b8cb2d9c072afb
Agustin Servin <agustinservin1@gmail.com>
2022-10-12 08:46:52 -0300

    agrego el js del buscador + estilo

--- libs/app.php
+++ libs/app.php
@@ -8,36 +8,38 @@ class App{
         $url = isset($_GET['url']) ? $_GET['url']:null; //---$URL  = si existe algo en get['url']lo guardo, sino guardo null.
         $url = rtrim($url,'/'); //elimina los / de mas
         $url = explode('/',$url); //convierte cada / en una posicion, ej: home/funcion/xcosa = 0/1/2
-    
+        
+        //si la url esta vacia, el controlador es home
         if(empty($url[0])){
-            $archivoController = 'controllers/Home.php';
+            $archivoController = 'controllers/home.php';
             require_once $archivoController;
             $controller = new Home();
             $controller->loadModel('home');
             $controller->render();
             return false;
         }
-    
+        //en caso d qu no ste vacia verifico que exista y la requiero
         $archivoController = 'controllers/'.$url[0].".php";
         if(file_exists($archivoController)){
         
             require_once $archivoController;
             $controller = new $url[0];
             $controller->loadModel($url[0]);
-
+            //si hay algo mas en  la url, ademas del controlador, estamos llamando un metodo
             if (isset($url[1])){
                 if(!method_exists($controller, $url[1])){
                     $controller = new Errors('404');
                     $controller->render();
                 }
                 else{
-                    $nparam = sizeof($url);
-                    if($nparam > 2){
-                        $param = [];
-                        for($i = 2; $i<$nparam;$i++){
-                            array_push($param, $url[$i]);
+                    //cargo todos los parametros que haya (se dividen por cada "/" que haya despues del metodo)
+                    $numParametros = sizeof($url);
+                    if($numParametros > 2){
+                        $parametros = [];
+                        for($i = 2; $i<$numParametros;$i++){
+                            array_push($parametros, $url[$i]);
                         }
-                        $controller->{$url[1]}($param);
+                        $controller->{$url[1]}($parametros);
                     }else{
                     $controller->{$url[1]}();
                     }
--- public/scripts/buscador.js
+++ public/scripts/buscador.js
@@ -1,7 +1,9 @@
-    
-    function buscar(){
-        escrito = document.querySelector('#form1');
+
+function buscar(){
+    const escrito = document.getElementById('form1');
+const btnBuscar = document.getElementById('btnBuscar');
         if(escrito.value != ""){
         window.location= "http://localhost/PRUEBAMVC/home/buscar/"+escrito.value;
-	}
+	}   
 }
+    
