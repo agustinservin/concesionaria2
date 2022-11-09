8901065f597b35db6b07eeb2f1255d6b83e6f2fa
Agustin Servin <agustinservin1@gmail.com>
2022-11-09 11:32:46 -0300

    modificando el menu responsive(falta terminar)

--- views/header.php
+++ views/header.php
@@ -12,8 +12,9 @@
     
     
     <div id="header">
+    <i id="menu-icono" class="fa-solid fa-bars"></i>
         <!--<img class="logo" src="<?php echo constant('URL');?>public/images/logo.png"></ing>
----><h2 class="logo">L O G O</h2>
+--->
 <ul>
         
         <a style="text-decoration:none;" class="link-header" href="<?php echo constant('URL')?>"><li class="item-header">Home</li></a>
@@ -22,6 +23,7 @@
 
         <a style="text-decoration:none;" class="link-header" href="<?php echo constant('URL')?>contacto"><li class="item-header">Contactanos</li></a>
         </ul>
+        <h2 class="logo">L O G O</h2>
     </div>
     
 </body>
